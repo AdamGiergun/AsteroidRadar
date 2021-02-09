3cf9ad79e9140c7229ed9044521c07085f5bd24b
Adam Giergun <adam.giergun@gmail.com>
2021-02-09 10:45:45 +0100

    Remove duplicated BASE_URL const from Service.kt

--- starter/app/src/main/java/com/udacity/asteroidradar/network/Service.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/network/Service.kt
@@ -2,15 +2,13 @@ package com.udacity.asteroidradar.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants.BASE_URL
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 enum class AsteroidsApiStatus  { LOADING, ERROR, DONE }
-
-private const val BASE_URL =
-    "https://api.nasa.gov/neo/rest/v1/"
 
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
@@ -22,7 +20,7 @@ private val retrofit = Retrofit.Builder()
     .build()
 
 interface AsteroidsApiService {
-    @GET("feed")
+    @GET("neo/rest/v1/feed")
     suspend fun getAsteroids(
         @Query("api_key") apiKey: String,
         @Query("start_date") startDate: String,
